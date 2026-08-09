6d09f4dbf9dce2dce6927f81637bf7fa14093632
ραтяιcкƒαηєƖƖα <patrick@subcult.tv>
2026-08-09 02:07:57 -0500

    fix: grant contact return and retention columns

--- db/migrations/0008_project_delivery_and_contact_retention.sql
+++ db/migrations/0008_project_delivery_and_contact_retention.sql
@@ -8,6 +8,7 @@ DO $$
 BEGIN
   IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'patrickfanella') THEN
     GRANT DELETE ON TABLE public.contact_messages TO patrickfanella;
+    GRANT SELECT (id, created_at) ON TABLE public.contact_messages TO patrickfanella;
   END IF;
 END
 $$;
